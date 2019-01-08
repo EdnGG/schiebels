37e55cc3371e2fa142d6d0d346702e165969b9c4
Eden <gresseden@gmail.com>
2019-01-07 22:13:14 -0500

    Hey there

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCoZVVWyHoaIHPWtuyKOb-_umlUZjV3DzE",
+    authDomain: "schiebels-fe493.firebaseapp.com",
+    databaseURL: "https://schiebels-fe493.firebaseio.com",
+    projectId: "schiebels-fe493",
+    storageBucket: "schiebels-fe493.appspot.com",
+    messagingSenderId: "485942152626"
+};
